7de25e3bb67858cb6ebce1b6cee4e7332f0f28c5
Nitay_Das <nitaydas910@gmail.com>
2024-05-06 17:17:51 +0600

    Adding phone Card

--- PhoneInfo-Hunter/js/index.js
+++ PhoneInfo-Hunter/js/index.js
@@ -4,9 +4,35 @@ const loadphone = async () =>{
     const res= await fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
     const data= await res.json();
 
-    const phone =data.data;
+    const phones =data.data;
 
-    console.log(phone);
+    // console.log(phones);
+
+    displayPhone(phones);
 }
 
 loadphone();
+
+
+displayPhone = phones =>{
+    phonecontainer=document.getElementById('phone-container');
+    phones.forEach(phone => {
+        // console.log(phone)
+
+        const phoneCard=document.createElement('div');
+        phoneCard.classList=`card p-4 bg-base-100 shadow-xl`;
+
+       phoneCard.innerHTML=`<figure><img src="${phone.image}" alt="Shoes" /></figure>
+                            <div class="card-body">
+                                <h2 class="card-title">${phone.phone_name}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>`;
+                                 
+        // appendcild
+        phonecontainer.appendChild(phoneCard);
+
+    });
+}
